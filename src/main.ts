8ca10fb95884cea3756d2f5c65b19dbd7a740794
eschirtz <eschirtzinger@gmail.com>
2025-05-06 22:08:53 -0700

    Add Firebase integration and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
 
 import App from './App.vue'
 import router from './router'
@@ -12,3 +14,19 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDh5-A0LCHWuwD8hIfaZphPj08j_C0OkKk',
+  authDomain: 'staring-in-spaces.firebaseapp.com',
+  projectId: 'staring-in-spaces',
+  storageBucket: 'staring-in-spaces.firebasestorage.app',
+  messagingSenderId: '104430158231',
+  appId: '1:104430158231:web:f78184ff5df97e61cc44d2',
+  measurementId: 'G-4VZW8XFNDB',
+}
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig)
+const analytics = getAnalytics(firebaseApp)
+
+console.log(analytics)
